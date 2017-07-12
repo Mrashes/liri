4411a66b41484aed263f99487ef81719291d6fc1
Max Rashes <mrashes2@gmail.com>
2017-07-12 14:02:44 -0500

    movie works

--- key.js
+++ key.js
@@ -4,8 +4,8 @@ exports.twitter = {
 	access_token_key: '257183792-JyP9sL5B5X7pLLoCsrVgWHJd3lEVbLGyUYSTPGXP',
 	access_token_secret: 'xsOW8ej4Glp1KMlwNaD7tEKtUmB9UHbuRf2XuAqu66Bbg',
 }
-exports.moviedb = {
-	key:'f440d9d199a99e977b1a9ab856abcd6f',
+exports.omdb = {
+	key:'40e9cece',
 }
 exports.spotify = {
 
--- liri.js
+++ liri.js
@@ -1,8 +1,7 @@
 const key = require('./key.js')
 const Twitter = require('twitter')
 // const omdb = require('omdb');
-const mdb = require('moviedb')(key.moviedb.key)
-const request = require("request");
+const request = require('request');
 
 if (process.argv[2] === 'trump-tweets'){
 	let T = new Twitter(key.twitter)
@@ -20,8 +19,11 @@ if (process.argv[2] === 'trump-tweets'){
 			// console.log(data)
 			console.log(`looking for ${params.q}`)
 			//for all the returns
+			// console.log(data.statuses[0].text)
 			for (let i=0; i<data.statuses.length; i++) {
-				console.log(data.statuses[i])
+				console.log(data.statuses[i]['text'])
+				console.log(data.statuses[i]['text'])
+				user.name
 			}
 		}
 		else {
@@ -33,37 +35,29 @@ else if (process.argv[2] === 'spotify-this-song') {
 
 }
 else if (process.argv[2] === 'movie-this') {
-	//get the movie id
-	let movieName = process.argv[3]
-	mdb.searchMovie({ query: movieName }, (err, res) => {
-	  console.log(res);
-	  // return 
-	});
+	let movieName = process.argv[3] || 'Mr. Nobody'
+	let keyOMDB = key.omdb.key
+	let url = `http://www.omdbapi.com/?apikey=${keyOMDB}&t=${movieName}`
 
+	request(url, function (err, response, body) {
+	    if(err){
+	      console.log('there was a problem with the API call : '+ err)
+	    }
+	    else {
+	    	let movie = JSON.parse(body)
+	    	// console.log(movie['Ratings'])
 
-	// var movieid = 121518;
-	// var options = { method: 'GET',
-	//   url: 'https://api.themoviedb.org/3/movie/'+movieid+'/credits',
-	//   qs: { api_key: key.moviedb.key },
-	//   body: '{}' };
+       		console.log('Title : '+movie['Title']);
+       		console.log('Year : '+movie['Year']);
+       		console.log('imdb Rating : '+movie['Ratings'][0]['Value']);
+       		console.log('Tomato Rating : '+movie['Ratings'][1]['Value']);
+       		console.log('Country Filmed in : '+movie['Country']);
+       		console.log('Original Language : '+movie['Language']);
+       		console.log('Plot : '+movie['Plot']);
+       		console.log('Actors : '+movie['Actors']);
+	    }
+	  });
 
-	// request(options, function (error, response, body) {
-	//   if (error) throw new Error(error);
-
-	//   console.log(body);
-	// });
-
-	//
-	// var options = { method: 'GET',
-	//   url: 'https://api.themoviedb.org/3/movie/'+movieid,
-	//   qs: { language: 'en-US', api_key: api_key: key.moviedb.key },
-	//   body: '{}' };
-
-	// request(options, function (error, response, body) {
-	//   if (error) throw new Error(error);
-
-	//   console.log(body);
-	// });
 }
 else if (process.argv[2] === 'do-what-it-says') {
 	
